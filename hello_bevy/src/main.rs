1568e5dffa8a904340505c85a1adb42de6ac7ca7
Richard Hoffmann <richard.hoffmann@scout24.com>
2025-10-26 15:47:39 +0100

    add some keyboard handli ng

--- hello_bevy/src/main.rs
+++ hello_bevy/src/main.rs
@@ -4,6 +4,32 @@ use bevy::sprite::Sprite;
 #[derive(Component)]
 struct Dragon;
 
+fn movement(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut dragon_query: Query<&mut Transform, With<Dragon>>,
+) {
+    let mut translate_delta = Vec2::ZERO;
+    let mut scale_delta = Vec2::ZERO;
+
+    // Pattern match on individual key presses
+    for key in keyboard.get_pressed() {
+        match key {
+            KeyCode::ArrowLeft | KeyCode::KeyA => translate_delta.x -= 1.0,
+            KeyCode::ArrowRight | KeyCode::KeyD => translate_delta.x += 1.0,
+            KeyCode::ArrowUp | KeyCode::KeyW => translate_delta.y += 1.0,
+            KeyCode::ArrowDown | KeyCode::KeyS => translate_delta.y -= 1.0,
+            KeyCode::Space => scale_delta += 0.05,
+            KeyCode::KeyQ => scale_delta -= 0.05,
+            _ => {}
+        }
+    }
+
+    dragon_query.iter_mut().for_each(|mut transform| {
+        transform.translation += translate_delta.extend(0.0);
+        transform.scale += scale_delta.extend(0.0);
+    })
+}
+
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d::default());
     let dragon_image = asset_server.load("dragon.png");
@@ -17,5 +43,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, movement)
         .run();
 }
